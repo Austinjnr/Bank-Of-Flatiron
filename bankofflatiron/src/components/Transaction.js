490328677ccb81071f8109c4b73f35c58f88c443
austinjnr <austinobonyo@gmail.com>
2023-01-17 07:28:33 +0300

    called the useEffect hook

--- bankofflatiron/src/components/Transaction.js
+++ bankofflatiron/src/components/Transaction.js
@@ -1,1 +1,24 @@
 import React, { useEffect, useState } from "react"
+import TransactionItem from "./TransactionItem"
+
+function Transaction (){
+
+    const [transaction, setTransaction] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:3000/transactions")
+        .then(r => r.json())
+        .then(transc => setTransaction(transc))
+        .catch(kaws => console.log(kaws))
+    },[])
+
+    return(
+        <div>
+            <table>
+                <tr>
+                    
+                </tr>
+            </table>
+        </div>
+    )
+}
