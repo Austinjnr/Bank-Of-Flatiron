ab140f7072dca1567191955a0ad5a25ed43b7c0b
austinjnr <austinobonyo@gmail.com>
2023-01-17 06:20:25 +0300

    Imported component files

--- bank/src/App.js
+++ bank/src/App.js
@@ -1,10 +1,19 @@
 //import logo from './logo.svg';
 import './App.css';
+import NewTransactionsform from './components/NewTransactionsform';
+import Searchform from './components/Searchform';
+import Transactions from './components/Transactions';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Bank Of Flatiron</h1>
+    <div className="ui raise segment">
+      <div className='header-text'>
+        <h2>The Royal Bank of Flatiron</h2>
+      </div>
+      <SearchForm/>
+      <NewTransactionsform/>
+      <Transactions/>
+      
     </div>
   );
 }
